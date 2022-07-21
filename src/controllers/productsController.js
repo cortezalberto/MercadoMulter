568e6f9459a2e3cbdca15cd8858eee39ac48f53b
cortezalberto <cortezalberto@gmail.com>
2022-07-21 18:42:19 -0300

    Primer commit

---
+++ src/controllers/productsController.js
@@ -0,0 +1,103 @@
+
+const jsonDB = require('../model/jsonDatabase');
+const productModel = jsonDB('products')
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+
+
+	detail: (req, res) => {
+		const product = productModel.find(req.params.id)
+		console.log("------------ESTOY EN DETAIL----------------------")
+		console.log(product)
+		console.log(product.image[0])
+		// COn este veo las otras fotos por eso el índice empieza en UNO Esto NO SIRVE !!!
+		console.log("VEO LAS SIGUIENTES FOTOS")
+		for( let i = 1; i < (product.image).length; i++ ) { 
+			console.log(product.image[i] )
+		
+		}
+		res.render('detail', {
+			product,
+			toThousand
+		})
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form')
+	},
+
+
+	// Create -  Method to store
+	
+	store: (req, res) => {
+		// Acá se trata como un array de files
+		let imagenes= []
+// leo secuencialmente el array de fotos y las cargo en el array de imágenes
+//  puede ser que venga una sóla foto
+        for(let i = 0 ; i<req.files.length;i++){
+            imagenes.push(req.files[i].filename)
+        }
+
+		
+		console.log(req.files);
+
+	
+		
+		// Atrapo todos los campos del formulario
+
+		const newProduct = {
+			...req.body,
+			// Si no mando imágenes pongo na por defecto
+			image:req.files != undefined?imagenes:"default.jpg"
+		}
+		productModel.create(newProduct)
+		console.log('cree un nuevo producto')
+		res.redirect('/')
+	},
+
+
+	// Update - Form to edit
+
+
+	edit: (req, res) => {
+		console.log('ESTOY USANDO EL EDIT DEL GENERICO')
+		let productToEdit = productModel.find(req.params.id)
+		console.log(productToEdit.image)
+		res.render('product-edit-form', { productToEdit })
+	},
+
+	// Update - Method to update
+
+
+
+	update: (req, res) => {
+		let productToEdit = productModel.find(req.params.id)
+
+		productToEdit = {
+
+			id: productToEdit.id,
+			...req.body,
+		//	image: productToEdit.image,
+
+		}
+
+		productModel.update(productToEdit)
+		res.redirect("/");
+
+	},
+
+
+    destroy: function(req,res){
+        productModel.delete(req.params.id);
+        res.redirect("/");
+    }
+
+
+
+};
+
+
+
+module.exports = controller;
